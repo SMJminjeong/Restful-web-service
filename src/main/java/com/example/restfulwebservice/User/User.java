9078227b84f86fe73115ce48dff0a8d39d462819
unknown <mjsong@spectra.co.kr>
2022-09-01 09:35:26 +0900

    git master branch test

--- src/main/java/com/example/restfulwebservice/User/User.java
+++ src/main/java/com/example/restfulwebservice/User/User.java
@@ -3,6 +3,7 @@ package com.example.restfulwebservice.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
@@ -14,5 +15,7 @@ public class User { //Lombok사용으로 간단하게 선언만 해줌
     // 유효성 체크
     @Size
     private String name;
+    //과거 데이터로 지정
+    @Past
     private Date joinDate;
 }
