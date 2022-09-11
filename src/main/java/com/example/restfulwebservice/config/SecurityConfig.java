5b39340e59b6ccc39ec6f3c8fce96fe88a276294
SMJminjeong <sdwp0920@gmail.com>
2022-09-11 23:28:24 +0900

    2022/09/11 test

--- src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
+++ src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -1,20 +1,21 @@
-package com.example.restfulwebservice.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-
-@Configuration
-//memory에 설정정보를 같이 로딩
-public class SecurityConfig extends WebSecurityConfiguration {
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth)
-        throws Exception{
-        auth.inMemoryAuthentication()
-                .withUser("kenneth")
-                .password("{noop}test1234") //{noop} 인코딩 없이 사용할 수 있는
-                .roles("USER");
-    }
-
-}
+//package com.example.restfulwebservice.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+//
+//@Configuration
+////memory에 설정정보를 같이 로딩
+////github test
+//public class SecurityConfig extends WebSecurityConfiguration {
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth)
+//        throws Exception{
+//        auth.inMemoryAuthentication()
+//                .withUser("kenneth")
+//                .password("{noop}test1234") //{noop} 인코딩 없이 사용할 수 있는
+//                .roles("USER");
+//    }
+//
+//}
